f6d0565d1a0a6a9dbcba4a83a3790ef29a223247
Sri karan reddy prodduturi <54560060+psrikaranreddy@users.noreply.github.com>
2020-04-23 23:31:36 -0400

    Add files via upload

---
+++ MyPortfolio/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  //$("#fadebutton").click(fade);
+  //$("#fadebutton").dblclick(fade);
+  //$("#fadebutton").mouseenter(fade);
+  //$("#fadebutton").mouseleave(fade);
+  //$("#fadebutton").hover(fade);
+    //$("#fadejquery").hide();
+    //$("#fadejquery").show();
+    //$("#fadejquery").toggle();
+    //$("#fadejquery").fadeout();
+    //$("#fadejquery").fadein();
+    //$("#fadejquery").fadeToggle();
+    //$("#fadejquery").slideUp();
+    //$("#fadejquery").slideDown();
+    //$("#fadejquery").slideToggle();
+    //var mytext = $("#fadejquery").text(); it will get the text for fadejquery id and store it in mytext
+    //$("#fadejquery").text("what <i>happend</i>") it will change the whole text of thet ID to what happend. it wil not italic the happend word but will print the tags
+    //$("#fadejquery").html() it will get the text but with the html tags. i.e if <b> tage in for a word that will come
+    //$("#fadejquery").html("what <i>happend</i>") it will changethe text to what happend with happend in italic
+    //var atribute_value = $("#heart").attr("width") it will fetch the attribute i.e width here value. wil fetch width value
+    //$("#heart").attr("width","150px") change the width to 150px
+    //$("#heart").css("font-size", "25px") it will set the css proprety
+  }
+});
